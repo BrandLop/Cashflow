a220e59795b11101a9cfabad9ad18eb1293bf792
BrandLop <brandon.lopezt@alumno.buap.mx>
2023-04-02 14:20:41 -0600

    Use splashscreen component in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <script>
-import Home from './components/Home.vue'
+import SplashScreen from './components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue'
+
 export default {
   components: {
-    Home
-  }
-}
+    SplashScreen,
+    Home: defineAsyncComponent(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import("./components/Home.vue"));
+        }, 1500);
+      });
+    })
+  },
+}; 
 </script>
 
 <template>
-  <Home/>
+  <Suspense>
+    <template #default>
+      <Home/>
+    </template>
+    <template #fallback>
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
 
-<style scoped></style>
+<style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
