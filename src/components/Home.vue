318df63689ec37df98e5d4c8ddffe9b76fbdab79
BrandLop <brandon.lopezt@alumno.buap.mx>
2023-04-03 00:19:41 -0600

    Bug fixes in home component and chart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
         :total-amount="totalAmount"
         :amount="amount"
       >
-        <template #chart> <ChartResume :amounts="amounts" /> </template>
+        <template #chart> <ChartResume :amounts="amounts" @select="select" /> </template>
         <template #action> <ActionHome @create="create" /> </template>
       </IndexHome>
     </template>
@@ -56,7 +56,7 @@ export default {
         .map((m) => m.amount)
 
       return lastDays.map((m, i) => {
-        const lastMovements = lastDays.slice(0, i)
+        const lastMovements = lastDays.slice(0, i + 1)
         return lastMovements.reduce((suma, movement) => {
           return suma + movement
         }, 0)
@@ -89,6 +89,10 @@ export default {
     },
     save() {
       localStorage.setItem('movements', JSON.stringify(this.movements))
+    },
+    select(el) {
+      console.log(el)
+      this.amount = el
     }
   }
 }
